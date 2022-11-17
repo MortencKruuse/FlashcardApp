5115f7da9aa7ceea1bc8ab5673cb6d12984194c6
Morten Kruuse <s183681@student.dtud.k>
2022-11-17 10:56:52 +0100

    Created compose files

--- app/src/main/java/com/example/flashcardapp/ui/FlipCardActivity.kt
+++ app/src/main/java/com/example/flashcardapp/ui/FlipCardActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flashcardapp
+package com.example.flashcardapp.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
---
+++ app/src/main/java/com/example/flashcardapp/ui/flipscreen/FlipScreen.kt
@@ -0,0 +1,8 @@
+package com.example.flashcardapp.ui.flipscreen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun flipScreen(){
+
+}
