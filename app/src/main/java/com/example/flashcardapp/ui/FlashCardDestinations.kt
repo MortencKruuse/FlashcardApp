d7322cd6a830d040a488e0728ffdce75f1affcba
Morten Kruuse <Mortenckruuse@gmail.com>
2022-11-26 15:38:58 +0100

    Added image

--- app/src/main/java/com/example/flashcardapp/ui/FlashCardDestinations.kt
+++ app/src/main/java/com/example/flashcardapp/ui/FlashCardDestinations.kt
@@ -2,12 +2,14 @@ package com.example.flashcardapp.ui
 
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBalance
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.PieChart
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.flashcardapp.ui.flashscreen.FlashScreen
 import com.example.flashcardapp.ui.mainscreen.MainScreen
+import com.example.flashcardapp.ui.deckscreen.DeckScreen
 
 interface FlashCardDestination {
     val icon: ImageVector
@@ -28,4 +30,11 @@ object FlashScreen : FlashCardDestination {
     override val screen: @Composable () -> Unit = { FlashScreen()}
 }
 
-val flashCardTabRowScreens = listOf(MainScreen, FlashScreen)
+object DeckScreen : FlashCardDestination {
+    override val icon = Icons.Default.AccountBalance
+    override val route = "deckscreen"
+    override val screen: @Composable () -> Unit = { DeckScreen() }
+}
+
+
+val flashCardTabRowScreens = listOf(MainScreen, FlashScreen,DeckScreen)
