cc56bcaf731c16171c2b23e00ceb30640548e598
Sejr_Yoga <s195481@student.dtu.dk>
2022-11-26 18:12:13 +0100

    Added     id "dagger.hilt.android.plugin"     to gradle in order to do dependency injections and coroutines in the long run

--- app/src/main/java/com/example/flashcardapp/ui/FlashCardActivity.kt
+++ app/src/main/java/com/example/flashcardapp/ui/FlashCardActivity.kt
@@ -6,31 +6,62 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import com.example.flashcardapp.ui.theme.FlashcardAppTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.example.flashcardapp.data.Card
 import com.example.flashcardapp.data.Deck
+import com.example.flashcardapp.data.repositories.DeckRepository
 import com.example.flashcardapp.data.viewmodels.DeckViewModel
-import com.example.flashcardapp.ui.components.Card
 import com.example.flashcardapp.ui.components.FlashCardTabRow
-
+import com.example.flashcardapp.ui.theme.FlashcardAppTheme
+import kotlinx.coroutines.flow.SharingStarted
+import javax.inject.Inject
 
 
 class FlashCardActivity : ComponentActivity() {
-    //TODO DB STUFF: Research appropriate location for line below
-    val commit = ""
-    private lateinit var deckViewModel: DeckViewModel
+
+    @Inject
+    lateinit var deckRepository: DeckRepository
+
+    val deckRepositoryState by lazy {
+        deckRepository.getDeck()
+                /*
+            .stateIn(
+                lifecycleScope,
+                started = SharingStarted.WhileSubscribed(),
+                emptyList()
+            )*/
+    }
+
+/*
+    //TODO DB STUFF: Research appropriate location for line below and if should be "lateInit var"
+    var deckViewModel: DeckViewModel = ViewModelProvider(this).get(DeckViewModel::class.java)
+
+    init {
+        val cards = mutableListOf<Card>(
+            Card(1, "Q", "A", 1),
+            Card(2, "Q", "A", 1),
+            Card(3, "Q", "A", 1),
+            Card(4, "Q", "A", 1),
+            Card(5, "Q", "A", 1),
+            Card(6, "Q", "A", 1),
+            Card(7, "Q", "A", 1),
+            Card(8, "Q", "A", 1),
+            Card(9, "Q", "A", 1),
+            Card(10, "Q", "A", 1)
+        )
+
+        val decks = mutableListOf<Deck>(
+            Deck(1, "DeckOne", cards)
+        )
+        deckViewModel.addDeck(decks.first())
+    }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            //TODO DB STUFF: maybe not here the viewmodel should be initialized. Research later on.
-            deckViewModel = ViewModelProvider(this).get(DeckViewModel::class.java)
             FlashCardApp()
 
         }
@@ -38,7 +69,7 @@ class FlashCardActivity : ComponentActivity() {
 }
 
 @Composable
-fun FlashCardApp(){
+fun FlashCardApp() {
     FlashcardAppTheme {
         //remember keeps a value (any value) consistent across recompositions.
         //mutableStateOf returns a MutableState. Think mutableListOf.
