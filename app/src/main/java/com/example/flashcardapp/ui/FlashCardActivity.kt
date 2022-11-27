a4b3f1306d648f5c3248b2f0777ae6758dc08bf4
Sejr_Yoga <s195481@student.dtu.dk>
2022-11-27 18:31:22 +0100

    Added viewModel to FlashCardActivity.kt So far so good

--- app/src/main/java/com/example/flashcardapp/ui/FlashCardActivity.kt
+++ app/src/main/java/com/example/flashcardapp/ui/FlashCardActivity.kt
@@ -44,34 +44,95 @@ class FlashCardActivity : ComponentActivity() {
             Surface(
                 modifier = Modifier.fillMaxSize(),
             ) {
-                FlashCardApp()
+                FlashcardAppTheme {
+                    // A surface container using the 'background' color from the theme
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colors.background
+                    ) {
+
+                        val owner = LocalViewModelStoreOwner.current
+
+                        owner?.let {
+                            val viewModel: DeckViewModel = viewModel(
+                                it,
+                                "MainViewModel",
+                                ViewModelFactory(
+                                    LocalContext.current.applicationContext
+                                            as Application
+                                )
+                            )
+
+                            ScreenSetup(viewModel)
+                        }
+                    }
+                }
             }
         }
     }
 }
-
-
-
 
 @Composable
-fun FlashCardApp() {
-    FlashcardAppTheme {
-        //remember keeps a value (any value) consistent across recompositions.
-        //mutableStateOf returns a MutableState. Think mutableListOf.
-        //MutableState is just a thing that holds a value, where Compose will automatically observe changes to the value. Think MutableLiveData, but you don't need to call observe yourself.
-        var currentScreen: FlashCardDestination by remember { mutableStateOf(MainScreen) }
-        Scaffold(
-            bottomBar = {
-                FlashCardTabRow(
-                    allScreens = flashCardTabRowScreens,
-                    onTabSelected = { screen -> currentScreen = screen },
-                    currentScreen = currentScreen
-                )
-            }
-        ) { innerPadding ->
-            Box(Modifier.padding(innerPadding)) {
-                currentScreen.screen()
-            }
+fun ScreenSetup(viewModel: DeckViewModel) {
+
+    val allDecks by viewModel.allDecks.observeAsState(listOf())
+    val searchResults by viewModel.searchResults.observeAsState(listOf())
+
+    MainScreen(
+        allDecks = allDecks,
+        searchResults = searchResults,
+        viewModel = viewModel
+    )
+}
+
+@Composable
+fun MainScreen(
+    allDecks: List<Deck>,
+    searchResults: List<Deck>,
+    viewModel: DeckViewModel
+) {
+    //remember keeps a value (any value) consistent across recompositions.
+    //mutableStateOf returns a MutableState. Think mutableListOf.
+    //MutableState is just a thing that holds a value, where Compose will automatically observe changes to the value. Think MutableLiveData, but you don't need to call observe yourself.
+    var currentScreen: FlashCardDestination by remember { mutableStateOf(MainScreen) }
+    Scaffold(
+        bottomBar = {
+            FlashCardTabRow(
+                allScreens = flashCardTabRowScreens,
+                onTabSelected = { screen -> currentScreen = screen },
+                currentScreen = currentScreen
+            )
+        }
+    ) { innerPadding ->
+        Box(Modifier.padding(innerPadding)) {
+            currentScreen.screen()
         }
     }
 }
+
+
+/*
+setContent {
+        RoomDemoTheme {
+            // A surface container using the 'background' color from the theme
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colors.background
+            ) {
+
+                val owner = LocalViewModelStoreOwner.current
+
+                owner?.let {
+                    val viewModel: MainViewModel = viewModel(
+                        it,
+                        "MainViewModel",
+                        MainViewModelFactory(
+                          LocalContext.current.applicationContext
+                                                      as Application)
+                    )
+
+                    ScreenSetup(viewModel)
+                }
+            }
+        }
+ */
